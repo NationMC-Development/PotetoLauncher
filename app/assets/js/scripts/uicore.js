08fb5911db97c4f3e8dc3f4ed63ad1e5bf660144
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2023-02-01 20:50:00 +0900

    exe

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -49,7 +49,7 @@ if(!isDev){
                 loggerAutoUpdaterSuccess.log('New update available', info.version)
                 
                 if(process.platform === 'darwin'){
-                    info.darwindownload = `https://github.com/NationMC-Development/PotetoLauncher/releases/download/v${info.version}/PotetoLauncher-setup-${info.version}.dmg`
+                    info.darwindownload = `https://github.com/NationMC-Development/PotetoLauncher/releases/download/v${info.version}/PotetoLauncher-setup-${info.version}.exe`
                     showUpdateUI(info)
                 }
                 
